42c1a5c162a6c4a4817d73d559410fee1a930b02
emmanuelmeena91 <emmanuel.meena@moringaschool.com>
2023-06-29 11:45:55 +0300

    control flow

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let greeting;
+  if (feet <= 400) {
+    greeting = 'This one is on me!';
+  } else if (feet > 2000 && feet <= 2500) {
+    greeting = 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    greeting = 'No can do.';
+  }
+  return greeting;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'Nairobi' ? 'Welcome to 254.' : 'still outside.';
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Boring.';
+    default:
+      return 'Bye.';
+  }
+
 }
